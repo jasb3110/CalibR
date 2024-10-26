a4408a033af9d0133c1e0f4176fd189d6ac4074a
jasb3110 <solisbenites.jose@gmail.com>
2024-10-25 22:51:35 -0500

    oka

--- prueba.R
+++ prueba.R
@@ -4,4 +4,4 @@ source("calib2.R")#Call up a function
 #INPUT
 #data is gonna calibrate
 d2=read.csv("mejillones2.csv",sep=";",dec=".",header = TRUE)
-calibR2(input=d2,sigma = 0.68,curve="marine20",show.table = F,show.plot =F,colour="yes")
+calibR2(input=d2,sigma = 0.68,curve="marine20",show.table = F,show.plot =F,colour="no")
